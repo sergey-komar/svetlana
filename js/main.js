671f6a434b72a0980668f7433de7dd7d77350dfe
sergey-komar <69416265+sergey-komar@users.noreply.github.com>
2021-03-05 22:48:02 +0300

    Add files via upload
    site

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $('.photography__tabs-item').on('click', function(e){
+        e.preventDefault();
+
+        $('.photography__tabs-item').removeClass('photography__tabs-item--active');
+        $('.photography__content-item').removeClass('photography__content-item--active');
+
+        $(this).addClass('photography__tabs-item--active');
+        $($(this).attr('href')).addClass('photography__content-item--active');
+    });
+
+    $('.mobile__button').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+
+});
